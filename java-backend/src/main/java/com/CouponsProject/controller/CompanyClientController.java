7184a652465f7a0313d8f94baecf27b7174bf388
Doron <doron.avitan.c@gmail.com>
2022-12-15 19:58:37 +0200

    clean code and comments CompanyClientController.java

--- java-backend/src/main/java/com/CouponsProject/controller/CompanyClientController.java
+++ java-backend/src/main/java/com/CouponsProject/controller/CompanyClientController.java
@@ -6,6 +6,7 @@ import com.CouponsProject.CouponSystemEntities.Coupon;
 
 import com.CouponsProject.service.ClientType;
 
+import lombok.EqualsAndHashCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -18,11 +19,13 @@ import java.util.List;
 
 import lombok.Data;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @Controller
 @RequestMapping("CouponApp")
 @CrossOrigin
 public class CompanyClientController extends ClientControllerAbs {
+
 
 
     //Login Company
@@ -31,6 +34,7 @@ public class CompanyClientController extends ClientControllerAbs {
     @Override
     //http://localhost:8080/CouponApp/login/Company/{clientType}/{email}/{password}
     public ResponseEntity<?> login(@PathVariable ClientType clientType, @PathVariable String email, @PathVariable String password) throws SQLException {
+        System.out.println("Using login function..."); // print to backend
 
         //will return the company ID if logged in successfully
         int companyIdToReturn = loginManager.loginM(clientType, email, password);
@@ -51,11 +55,15 @@ public class CompanyClientController extends ClientControllerAbs {
     }
 
 
+
     // Add Coupon to company
     @PostMapping("/addCoupon")
     @ResponseBody
     public ResponseEntity<?> addCoupon(@RequestBody Coupon coupon) throws Exception { // http://localhost:8080/CouponApp/addCoupon
+        System.out.println("Using addCoupon function..."); // print to backend
+
         Coupon res = companyService.addCoupon(coupon);
+
         //check if coupon added successfully
         if (res != null) {
             System.out.println("Coupon added successfully!"); // print to backend
@@ -71,10 +79,13 @@ public class CompanyClientController extends ClientControllerAbs {
     }
 
 
+
     // Update coupon
     @PutMapping("/updateCoupon/")
     @ResponseBody
     public ResponseEntity<?> updateCoupon(@RequestBody Coupon coupon) { // http://localhost:8080/CouponApp/updateCoupon/
+        System.out.println("Using updateCoupon function..."); // print to backend
+
         //check if coupon exist before updating
         if (companyService.findCouponById(coupon.getId()) != null) {
             //check if the future updated coupon belong to the company (cant update other companies coupons - only the coupons that belong to the company)
@@ -93,10 +104,13 @@ public class CompanyClientController extends ClientControllerAbs {
     }
 
 
+
     // Delete coupon
     @DeleteMapping("/deleteCouponById/{couponID}/{companyID}")
     @ResponseBody
     public ResponseEntity<?> deleteCouponById(@PathVariable int couponID, @PathVariable int companyID) throws Exception { // http://localhost:8080/CouponApp/deleteCouponById/{id}
+        System.out.println("Using deleteCouponById function..."); // print to backend
+
         //check if coupon exist before trying to delete it
         if (companyService.findCouponById(couponID) != null) {
             //check if the future deleted coupon belong to the company (cant delete other companies coupons - only the coupons that belong to the company)
@@ -117,12 +131,16 @@ public class CompanyClientController extends ClientControllerAbs {
     }
 
 
+
     //get all specific company coupons
     @GetMapping("/findCompanyCouponsByCompanyId/{id}")
     @ResponseBody
     public ResponseEntity<?> getAllCompanyCoupons(@PathVariable int id) throws Exception { // http://localhost:8080/CouponApp/findCompanyCouponsByCompanyId/{id}
+        System.out.println("Using getAllCompanyCoupons function..."); // print to backend
+
         //making a coupons list by company id
         List<Coupon> res = companyService.findByCompanyId(id);
+
         //check if company has any coupons
         if (!res.isEmpty()) {
             System.out.println("All company coupons from DB :"); // print to backend
@@ -140,12 +158,16 @@ public class CompanyClientController extends ClientControllerAbs {
     }
 
 
+
     //get all specific company coupons with category
     @GetMapping("/findCompanyCouponsByCompanyIdAndCategory/{id}/{category}")
     @ResponseBody
     public ResponseEntity<?> findCompanyCouponsByCompanyIdAndCategory(@PathVariable int id, @PathVariable Category category) throws Exception { // http://localhost:8080/CouponApp/findCompanyCouponsByCompanyIdAndCategory/{id}/{category}
+        System.out.println("Using findCompanyCouponsByCompanyIdAndCategory function..."); // print to backend
+
         //making a coupons list by company id and specific category
         List<Coupon> res = companyService.findByCompanyIdAndCategory(id, category);
+
         //check if company has any coupons by selected category
         if (!res.isEmpty()) {
             System.out.println("All company coupons from DB with category: " + category.toString().toLowerCase()); // print to backend
@@ -163,12 +185,16 @@ public class CompanyClientController extends ClientControllerAbs {
     }
 
 
+
     //get all specific company coupons by max price
     @GetMapping("/findCompanyCouponsByCompanyIdAndMaxPrice/{id}/{price}") //
     @ResponseBody
     public ResponseEntity<?> findCompanyCouponsByCompanyIdAndMaxPrice(@PathVariable int id, @PathVariable double price) throws Exception { // http://localhost:8080/CouponApp/findCompanyCouponsByCompanyIdAndMaxPrice/{id}/{price}
+        System.out.println("Using findCompanyCouponsByCompanyIdAndMaxPrice function..."); // print to backend
+
         //making a coupons list by company id and specific price
         List<Coupon> res = companyService.findByCompanyIDAndPriceLessThanEqual(id, price);
+
         //check if company has any coupons by selected price
         if (!res.isEmpty()) {
             System.out.println("All company coupons from DB with max price of: " + price); // print to backend
@@ -187,11 +213,15 @@ public class CompanyClientController extends ClientControllerAbs {
     }
 
 
+
     // Find coupon by ID
     @GetMapping("/findCouponByID/{id}")  // http://localhost:8080/CouponApp/findCouponByID/{id}
     @ResponseBody
     public ResponseEntity<?> getCouponById(@PathVariable int id) throws Exception {
+        System.out.println("Using getCouponById function..."); // print to backend
+
         Coupon res = companyService.findCouponById(id);
+
         //check if coupon exist
         if (res != null) {
             System.out.println("Coupon from DB by ID: " + id); // print to backend
@@ -209,11 +239,15 @@ public class CompanyClientController extends ClientControllerAbs {
     }
 
 
+
     // Get company details
     @GetMapping("/getCompanyDetailsById/{id}")  // http://localhost:8080/CouponApp/getCompanyDetailsById/{id}
     @ResponseBody
     public ResponseEntity<?> getCompanyById(@PathVariable int id) throws Exception {
+        System.out.println("Using getCompanyById function..."); // print to backend
+
         Company res = companyService.getCompanyById(id);
+
         //check if company exist
         if (res != null) {
             System.out.println("Company from DB by ID: " + id); // print to backend
