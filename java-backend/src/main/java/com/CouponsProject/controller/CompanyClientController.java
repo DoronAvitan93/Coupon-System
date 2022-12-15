89a1e05df0643126d2440136b22bad59050c580a
Doron <doron.avitan.c@gmail.com>
2022-12-15 19:27:31 +0200

    clean code and comments CompanyClientController.java

--- java-backend/src/main/java/com/CouponsProject/controller/CompanyClientController.java
+++ java-backend/src/main/java/com/CouponsProject/controller/CompanyClientController.java
@@ -32,33 +32,37 @@ public class CompanyClientController extends ClientControllerAbs {
     //http://localhost:8080/CouponApp/login/Company/{clientType}/{email}/{password}
     public ResponseEntity<?> login(@PathVariable ClientType clientType, @PathVariable String email, @PathVariable String password) throws SQLException {
 
+        //will return the company ID if logged in successfully
         int companyIdToReturn = loginManager.loginM(clientType, email, password);
-        if (companyIdToReturn > 0) {
-
+        //check if logged in successfully (-1 = error)
+        if (companyIdToReturn != -1) {
             System.out.println("Company Logged Successfully"); // print to backend
             System.out.println();
             ResponseEntity<Integer> responseWrapper = new ResponseEntity<>(companyIdToReturn, HttpStatus.OK); // print to client
             return responseWrapper;
+
+        } else {
+            System.out.println("Company login Error - incorrect email / password"); // print to backend
+            System.out.println();
+            ResponseEntity<String> responseWrapper = new ResponseEntity<>("Company login Error - incorrect email / password ", HttpStatus.BAD_REQUEST); // print to client
+
+            return responseWrapper;
         }
-
-        System.out.println("Company login Error - incorrect email / password"); // print to backend
-        System.out.println();
-        ResponseEntity<String> responseWrapper = new ResponseEntity<>("Company login Error - incorrect email / password ", HttpStatus.BAD_REQUEST); // print to client
-
-        return responseWrapper;
     }
 
 
-    // Add Coupon
+    // Add Coupon to company
     @PostMapping("/addCoupon")
     @ResponseBody
     public ResponseEntity<?> addCoupon(@RequestBody Coupon coupon) throws Exception { // http://localhost:8080/CouponApp/addCoupon
-        Coupon res = companyService.addCoupon(coupon); //main function.
+        Coupon res = companyService.addCoupon(coupon);
+        //check if coupon added successfully
         if (res != null) {
             System.out.println("Coupon added successfully!"); // print to backend
             System.out.println(coupon);
             ResponseEntity<?> response = new ResponseEntity<>("Coupon added successfully! " + coupon, HttpStatus.OK); // print to the client
             return response;
+
         } else {
             System.out.println("Cant add coupon - title already exist."); // print to backend
             ResponseEntity<?> response = new ResponseEntity<>("Cant add coupon - title already exist.", HttpStatus.BAD_REQUEST); // print to the client
@@ -71,14 +75,17 @@ public class CompanyClientController extends ClientControllerAbs {
     @PutMapping("/updateCoupon/")
     @ResponseBody
     public ResponseEntity<?> updateCoupon(@RequestBody Coupon coupon) { // http://localhost:8080/CouponApp/updateCoupon/
+        //check if coupon exist before updating
         if (companyService.findCouponById(coupon.getId()) != null) {
+            //check if the future updated coupon belong to the company (cant update other companies coupons - only the coupons that belong to the company)
             if (companyService.findCouponById(coupon.getId()).getCompany().getId() == coupon.getCompany().getId()) {
-
                 companyService.updateCoupon(coupon);
                 System.out.println("Coupon updated successfully! " + companyService.findCouponById(coupon.getId())); // print to backend
                 ResponseEntity<?> response = new ResponseEntity<>("Coupon updated successfully! " + companyService.findCouponById(coupon.getId()), HttpStatus.OK);
                 return response;
             }
+
+            //if coupon doesn't exist or doesn't belong to the company
         }
         System.out.println("No coupon exist by this ID: " + coupon.getId()); //print to backend
         ResponseEntity<?> response = new ResponseEntity<>("No coupon exist by this ID: " + coupon.getId(), HttpStatus.BAD_REQUEST);
@@ -90,7 +97,9 @@ public class CompanyClientController extends ClientControllerAbs {
     @DeleteMapping("/deleteCouponById/{couponID}/{companyID}")
     @ResponseBody
     public ResponseEntity<?> deleteCouponById(@PathVariable int couponID, @PathVariable int companyID) throws Exception { // http://localhost:8080/CouponApp/deleteCouponById/{id}
+        //check if coupon exist before trying to delete it
         if (companyService.findCouponById(couponID) != null) {
+            //check if the future deleted coupon belong to the company (cant delete other companies coupons - only the coupons that belong to the company)
             if (companyService.findCouponById(couponID).getCompany().getId() == companyID) {
                 companyService.deleteCouponById(couponID);
                 System.out.println("Coupon deleted Successfully!"); // print to backend
@@ -98,6 +107,8 @@ public class CompanyClientController extends ClientControllerAbs {
                 ResponseEntity<String> responseWrapper = new ResponseEntity<>("Coupon deleted successfully! ", HttpStatus.OK); // print to client
                 return responseWrapper;
             }
+
+            //if coupon doesn't exist or doesn't belong to the company
         }
         ResponseEntity<String> responseWrapper = new ResponseEntity<>("Error, no coupon exist by this ID ", HttpStatus.BAD_REQUEST); // print to client
         System.out.println("Error, no coupon exist by this ID: " + couponID); // print to backend
@@ -110,19 +121,22 @@ public class CompanyClientController extends ClientControllerAbs {
     @GetMapping("/findCompanyCouponsByCompanyId/{id}")
     @ResponseBody
     public ResponseEntity<?> getAllCompanyCoupons(@PathVariable int id) throws Exception { // http://localhost:8080/CouponApp/findCompanyCouponsByCompanyId/{id}
-        //find coupons by company id
+        //making a coupons list by company id
         List<Coupon> res = companyService.findByCompanyId(id);
+        //check if company has any coupons
         if (!res.isEmpty()) {
             System.out.println("All company coupons from DB :"); // print to backend
             System.out.println(res); // print to backend
             System.out.println();
             ResponseEntity<?> responseWrapper = new ResponseEntity<>(res, HttpStatus.OK);
             return responseWrapper;
+
+        } else {
+            System.out.println("No coupons exist for this company: "); // return to backend
+            System.out.println();
+            ResponseEntity<String> responseWrapper = new ResponseEntity<>("No coupons exist for this company: ", HttpStatus.BAD_REQUEST); // return to client
+            return responseWrapper;
         }
-        System.out.println("No coupons exist for this company: "); // return to backend
-        System.out.println();
-        ResponseEntity<String> responseWrapper = new ResponseEntity<>("No coupons exist for this company: ", HttpStatus.BAD_REQUEST); // return to client
-        return responseWrapper;
     }
 
 
@@ -130,18 +144,22 @@ public class CompanyClientController extends ClientControllerAbs {
     @GetMapping("/findCompanyCouponsByCompanyIdAndCategory/{id}/{category}")
     @ResponseBody
     public ResponseEntity<?> findCompanyCouponsByCompanyIdAndCategory(@PathVariable int id, @PathVariable Category category) throws Exception { // http://localhost:8080/CouponApp/findCompanyCouponsByCompanyIdAndCategory/{id}/{category}
+        //making a coupons list by company id and specific category
         List<Coupon> res = companyService.findByCompanyIdAndCategory(id, category);
+        //check if company has any coupons by selected category
         if (!res.isEmpty()) {
             System.out.println("All company coupons from DB with category: " + category.toString().toLowerCase()); // print to backend
             System.out.println(res); // print to backend
             System.out.println();
             ResponseEntity<List<Coupon>> responseWrapper = new ResponseEntity<>(res, HttpStatus.OK);
             return responseWrapper;
+
+        } else {
+            System.out.println("No coupons exist for this company: "); // return to backend
+            System.out.println();
+            ResponseEntity<?> responseWrapper = new ResponseEntity<>("No coupons exist for this company with category : " + category.toString().toLowerCase(), HttpStatus.BAD_REQUEST); // return to client
+            return responseWrapper;
         }
-        System.out.println("No coupons exist for this company: "); // return to backend
-        System.out.println();
-        ResponseEntity<?> responseWrapper = new ResponseEntity<>("No coupons exist for this company with category : " + category.toString().toLowerCase(), HttpStatus.BAD_REQUEST); // return to client
-        return responseWrapper;
     }
 
 
@@ -149,7 +167,9 @@ public class CompanyClientController extends ClientControllerAbs {
     @GetMapping("/findCompanyCouponsByCompanyIdAndMaxPrice/{id}/{price}") //
     @ResponseBody
     public ResponseEntity<?> findCompanyCouponsByCompanyIdAndMaxPrice(@PathVariable int id, @PathVariable double price) throws Exception { // http://localhost:8080/CouponApp/findCompanyCouponsByCompanyIdAndMaxPrice/{id}/{price}
+        //making a coupons list by company id and specific price
         List<Coupon> res = companyService.findByCompanyIDAndPriceLessThanEqual(id, price);
+        //check if company has any coupons by selected price
         if (!res.isEmpty()) {
             System.out.println("All company coupons from DB with max price of: " + price); // print to backend
             System.out.println("Coupons amount found: " + res.size());
@@ -157,11 +177,13 @@ public class CompanyClientController extends ClientControllerAbs {
             System.out.println();
             ResponseEntity<List<Coupon>> responseWrapper = new ResponseEntity<>(res, HttpStatus.OK);
             return responseWrapper;
+
+        } else {
+            System.out.println("No coupons exist for this company with max price of: " + price); // return to backend
+            System.out.println();
+            ResponseEntity<?> responseWrapper = new ResponseEntity<>("No coupons exist for this company with max price of :" + price, HttpStatus.BAD_REQUEST); // return to client
+            return responseWrapper;
         }
-        System.out.println("No coupons exist for this company with max price of: " + price); // return to backend
-        System.out.println();
-        ResponseEntity<?> responseWrapper = new ResponseEntity<>("No coupons exist for this company with max price of :" + price, HttpStatus.BAD_REQUEST); // return to client
-        return responseWrapper;
     }
 
 
@@ -170,17 +192,20 @@ public class CompanyClientController extends ClientControllerAbs {
     @ResponseBody
     public ResponseEntity<?> getCouponById(@PathVariable int id) throws Exception {
         Coupon res = companyService.findCouponById(id);
+        //check if coupon exist
         if (res != null) {
             System.out.println("Coupon from DB by ID: " + id); // print to backend
             System.out.println(res); // print to backend
             System.out.println();
             ResponseEntity<Coupon> responseWrapper = new ResponseEntity<>(res, HttpStatus.OK);
             return responseWrapper;
+
+        } else {
+            System.out.println("No coupon exist by this ID: " + id); // return to backend
+            System.out.println();
+            ResponseEntity<?> responseWrapper = new ResponseEntity<>("Error, no coupon exist by this ID ", HttpStatus.BAD_REQUEST); // return to client
+            return responseWrapper;
         }
-        System.out.println("No coupon exist by this ID: " + id); // return to backend
-        System.out.println();
-        ResponseEntity<?> responseWrapper = new ResponseEntity<>("Error, no coupon exist by this ID ", HttpStatus.BAD_REQUEST); // return to client
-        return responseWrapper;
     }
 
 
@@ -189,16 +214,19 @@ public class CompanyClientController extends ClientControllerAbs {
     @ResponseBody
     public ResponseEntity<?> getCompanyById(@PathVariable int id) throws Exception {
         Company res = companyService.getCompanyById(id);
+        //check if company exist
         if (res != null) {
             System.out.println("Company from DB by ID: " + id); // print to backend
             System.out.println(res); // print to backend
             System.out.println();
             ResponseEntity<Company> responseWrapper = new ResponseEntity<>(res, HttpStatus.OK);
             return responseWrapper;
+
+        } else {
+            System.out.println("No company exist by this ID: " + id); // return to backend
+            System.out.println();
+            ResponseEntity<?> responseWrapper = new ResponseEntity<>("Error, no company exist by this ID ", HttpStatus.BAD_REQUEST); // return to client
+            return responseWrapper;
         }
-        System.out.println("No company exist by this ID: " + id); // return to backend
-        System.out.println();
-        ResponseEntity<?> responseWrapper = new ResponseEntity<>("Error, no companyzcxz exist by this ID ", HttpStatus.BAD_REQUEST); // return to client
-        return responseWrapper;
     }
 }
