629c0d2cc0e03b60626875dd36fdd1b69a9bf167
Doron <doron.avitan.c@gmail.com>
2023-01-05 14:07:49 +0200

    little changes to help frontend

--- react-frontend/src/components/header/About.tsx
+++ react-frontend/src/components/header/About.tsx
@@ -1,37 +1,36 @@
 import { Fragment } from 'react';
-import Footer from '../footer/Footer';
-
-import Card from '../UI/Card';
 import './About.css'
 
 
 
-type Props = {
+const About = () => {
 
-}
-
-
-const About = (props) => {
 
 
 
 
     return (
         <Fragment >
-            <Card>
-                <h1>Welcome !</h1>
-                <h2>Hello, my name is Doron, I'm full stack graduate with experience in:</h2>
-                <h3> Java, Java SpringBoot, mySql, JavaScript, TypeScript, HTML, CSS, React.</h3>
-                <h3>My project - Coupon system, is a web app for companies who want to publish their coupons to customers.</h3>
-                <p>Coupon system is built with Java SpringBoot, MySql JavaScript & ReactJS</p>
-                <p>I'm constantly learning new things, currently those things include gaining more experience with:</p>
-                <p>Java, JavaSpringBoot, mySql and ReactJS</p>
-            </Card>
+            <div className='about-container'>
+                <h1 className='about-header'>Welcome !</h1>
+                <h2 className='about-text'>Hello, my name is Doron, I'm full stack graduate with experience in:</h2>
+                <h3 className='about-text'> Java, Spring Framework, SpringMVC, JPA, mySQL, HTML, CSS, Java script, TypeScript, React.</h3>
+                <h3 className='about-text'>My project - Coupon system, is a web app for both customers and companies.</h3>
+                <h3 className='about-text'>Coupon system is built with Java + Spring Framework for the backeEnd, MySql for the DB and ReactJS for the frontEnd </h3>
+                <br />
 
 
+                <img className='logo-react' width='60px' src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" alt="React logo" />
+                <img className='logo-spring' width='100px' src="https://w7.pngwing.com/pngs/713/936/png-transparent-spring-framework-representational-state-transfer-java-api-for-restful-web-services-microservices-others-text-trademark-logo.png" alt="Spring logo" />
+                <img className='logo-mysql' width='100px' src="https://cdn.freebiesupply.com/logos/large/2x/mysql-logo-svg-vector.svg" alt="mysql logo" />
 
+                <p className='about-text'>I'm constantly learning new things, currently those things include gaining more experience.</p>
+                <p className='about-text'>I carry out each project I engage in with MOTIVATION & DEDICATION, always striving to deliver my work on the highest standards</p>
+                <p className='about-text'> i have a very high self-study ability with a passion to learn and succeed.</p>
+            </div>
         </Fragment >
     );
 };
+
 
 export default About;
