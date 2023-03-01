021f3db27b5b94b7cc50d01ccd785557b01e47af
Doron <doron.avitan.c@gmail.com>
2023-03-01 21:37:38 +0200

    CSS

---
+++ react-frontend/src/components/main/MainHeader.tsx
@@ -0,0 +1,32 @@
+import React, { Fragment } from 'react';
+import Navigation from './Navigation';
+import './MainHeader.css'
+
+
+
+
+
+type Props = {
+
+
+}
+
+const MainHeader: React.FC<Props> = () => {
+
+
+  return (
+    <Fragment>
+
+      <div className='mainHeader'>
+
+        {/* //All the buttons here */}
+        <Navigation />
+
+
+      </div>
+    </Fragment >
+  );
+};
+
+
+export default MainHeader;
